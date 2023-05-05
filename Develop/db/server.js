84eba04dca708c721ff06c841e804ac98c4797bf
valerierobles <roblesv08@gmail.com>
2023-05-05 18:41:31 -0500

    added app.listen

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -12,9 +12,18 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", api);
 
-app.get("/notes", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+}); 
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
+
 
 
 
